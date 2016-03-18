d99ebd3c82dee7e35120af74fa0ff2a313e58a16
SaiKarthik Parachi <p.saikarthik@gmail.com>
2016-03-18 19:46:47 -0400

    Added Gulp, Wiredep
    interim checkin to perform front end dependency injection using bower,
    wiredep.
    Added gulp for performing node monitor and run the app.

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 var express = require('express');
-var message = "hello Brackets";
 
-if (true) {
-    console.log(message);
-}
+//var message = "hello Brackets";
+//
+//if (true) {
+//    console.log(message);
+//} else {
+//    console.log("hi");
+//}
 
-console.log("hi");
+var testApp = express();
+var portToListen = process.env.PORT || 3000;
+
+testApp.use(express.static('public'));
+//testApp.use(express.static('src/views'));
+testApp.set('views', './src/views');
+testApp.set('view engine', 'jade');
+
+testApp.get('/', function (req, res) {
+	res.render('index');
+});
+
+testApp.listen(portToListen, function (err) {
+	if (err) {
+		console.log('failed to listen on port ' + portToListen);
+	} else {
+		console.log('running on port ' + portToListen);
+	}
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
+
+var jsFilesToCheck = ['*.js', 'src/*/*.js'];
+var filesToMonitor = ['*.js', 'src/*/*.js'];
+
+gulp.task('style', function () {
+
+	return gulp.src(jsFilesToCheck)
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish', {
+			verbose: true
+		}))
+		.pipe(jscs());
+});
+
+gulp.task('inject', function () {
+
+	var wiredep = require('wiredep').stream;
+	var customFileInjection = require('gulp-inject');
+
+	var customFilesSource = gulp.src(['./public/css/*.css', './public/js/*.js'], {
+		read: false
+	});
+
+	var customFileOptions = {
+		ignorePath: '/public'
+	};
+
+	var options = {
+		bowerJson: require('./bower.json'),
+		directory: './public/lib',
+		//to avoid wiredep spit the entire path in our html/jade file use 'ignorePath' attribute
+		ignorePath: '../../public'
+	};
+
+	return gulp.src('./src/views/*.jade') // changing from *.html to *.jade
+		.pipe(wiredep(options))
+		.pipe(customFileInjection(customFilesSource, customFileOptions))
+		.pipe(gulp.dest('./src/views'));
+
+});
+
+//gulp tasks can be a pre-cursor to other gulp tasks, and can be mentioned as arguments
+//as they run asynchronously, excercise caution if they are updating same file
+//make sure to chain them if you have dependencies
+
+gulp.task('serveApp', ['style', 'inject'], function () {
+
+	var serveAppOptions = {
+		script: 'app.js',
+		delayTime: '50',
+		env: {
+			'PORT': 3030
+		},
+		watch: filesToMonitor
+	};
+
+	return nodemon(serveAppOptions)
+		.on('restart', function (args) {
+			console.log('server restarting..');
+		});
+
+});
